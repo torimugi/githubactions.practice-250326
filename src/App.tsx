1ab2fa717db3f3299da2b5ccc1271e0a782f3cf0
torimugi <zoo.zoo.0903@outlook.jp>
2025-03-28 18:06:59 +0900

    構造を作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,26 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <div>TODOを入力</div>
-      <div>TODOです</div>
-      <div>TODOです</div>
+      <div>
+        <input type="text" placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul className='flex mx-2 gap-2'>
+          <li className='flex-grow'>TODOです</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div>
+        <p>完了したTODO</p>
+        <ul>
+          <li>TODOです</li>
+          <button>戻す</button>
+          <button>削除</button>
+        </ul>
+      </div>
     </>
   )
 }
