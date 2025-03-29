324e38c42e494de25dc528bc17e11f360f62ca61
torimugi <zoo.zoo.0903@outlook.jp>
2025-03-29 19:48:41 +0900

    TaillwindCSSの再設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import "tailwindcss";
 
 const App = () => {
   return (
@@ -7,20 +7,19 @@ const App = () => {
         <input type="text" placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
-        <ul className='flex mx-2 gap-2'>
-          <li className='flex flex-grow'>TODOです</li>
+      <div className='mx-2'>
+        <p className='text-2xl font-bold'>未完了のTODO</p>
+        <ul className='flex items-center mx-2 gap-2'>
+          <li className='flex-grow'>TODOです</li>
           <button>完了</button>
           <button>削除</button>
         </ul>
       </div>
       <div>
         <p>完了したTODO</p>
-        <ul className='flex mx-2 gap-2'>
-          <li className='flex flex-grow'>TODOです</li>
+        <ul className='flex items-center mx-2 gap-2'>
+          <li className='flex-grow'>TODOでした</li>
           <button>戻す</button>
-          <button>削除</button>
         </ul>
       </div>
     </>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // TailwindCSSを適用するファイルを指定
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
