c4552d2e557be4bf0836da0432223d2fc1786cd6
torimugi <zoo.zoo.0903@outlook.jp>
2025-04-17 18:04:49 +0900

    未完了のTODO、map関数で実行する

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import "tailwindcss";
 
 export const App = () => {
- const [ Incompletes, setIncompletes ] = useState(["TODOです1", "TODOです2"]);
+ const [ Incompletes, setIncompletes ] = useState([
+  "TODOです1", 
+  "TODOです2"
+]);
 
   return (
     <>
@@ -14,17 +17,16 @@ export const App = () => {
       <div className='mx-2 rounded-md border-2 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>未完了のTODO</p>
         <ul className='flex-col items-center mx-2 gap-2'>
-          {Incompletes.map(() => {
-            return(
-              <li className='flex items-center gap-2'>
-              <p className="flex-grow">TODOです</p>
+          {Incompletes.map((todo) => (
+              <li key={todo} className='flex items-center gap-2'>
+              <p className="flex-grow">{todo}</p>
               <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
               <button className="bg-gray-200 hover:bg-gray-300">削除</button>
               </li>
-            )
-          })}
+          ))}
         </ul>
       </div>
+      
       <div className='mx-2 rounded-md border-2 bg-teal-400 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>完了のTODO</p>
         <ul className='flex items-center mx-2 gap-2'>
