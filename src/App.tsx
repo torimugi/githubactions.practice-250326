0c2f2a94767bf58533bc48307b3b4c6c36d1ed39
torimugi <zoo.zoo.0903@outlook.jp>
2025-04-21 18:07:16 +0900

    完了のTODO、map関数で実行する

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ export const App = () => {
   "TODOです2"
 ]);
 
+const [ Completes, setCompletes ] = useState([
+  "TODOでした1", 
+  "TODOでした2"
+]);
+
   return (
     <>
     <div className="flex flex-col gap-y-6">
@@ -29,15 +34,13 @@ export const App = () => {
       
       <div className='mx-2 rounded-md border-2 bg-teal-400 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>完了のTODO</p>
-        <ul className='flex items-center mx-2 gap-2'>
-          <li className='flex-grow'>TODOです</li>
-          <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
-          <button className="bg-gray-200 hover:bg-gray-300">戻す</button>
-        </ul>
-        <ul className='flex items-center mx-2 gap-2'>
-          <li className='flex-grow'>TODOです</li>
-          <button className="bg-gray-200 hover:bg-gray-300">完了</button>
-          <button className="bg-gray-200 hover:bg-gray-300">戻す</button>
+        <ul className='flex-col items-center mx-2 gap-2'>
+          {Completes.map((todo) => (
+              <li key={todo} className='flex items-center gap-2'>
+              <p className="flex-grow">{todo}</p>
+              <button className="bg-gray-200 my-2 hover:bg-gray-300">戻す</button>
+              </li>
+          ))}
         </ul>
       </div>
     </div>
