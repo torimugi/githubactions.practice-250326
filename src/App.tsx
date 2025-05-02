e43862d91f543897d3b23bd0f6122aa65afba9b9
torimugi <zoo.zoo.0903@outlook.jp>
2025-05-02 18:15:18 +0900

    削除機能を実装（indexを取得する）

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,9 @@ setTodoText("")
 console.log(handleAddTodo);
 }
 
-
+const handleDelete = (index: number) => {
+  alert(index);
+}
 
   return (
     <>
@@ -42,11 +44,12 @@ console.log(handleAddTodo);
       <div className='mx-2 rounded-md border-2 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>未完了のTODO</p>
         <ul className='flex-col items-center mx-2 gap-2'>
-          {Incompletes.map((todo) => (
+          {Incompletes.map((todo , index) => (
               <li key={todo} className='flex items-center gap-2'>
               <p className="flex-grow">{todo}</p>
               <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
-              <button className="bg-gray-200 hover:bg-gray-300">削除</button>
+              <button onClick={() => handleDelete(index)} 
+              className="bg-gray-200 hover:bg-gray-300">削除</button>
               </li>
           ))}
         </ul>
