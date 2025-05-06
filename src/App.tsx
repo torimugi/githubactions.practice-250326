011d071a87317d8d8a5a00a1e9f0fa8449132242
torimugi <zoo.zoo.0903@outlook.jp>
2025-05-06 15:19:26 +0900

    タクスの追加機能を実装する

--- src/App.tsx
+++ src/App.tsx
@@ -16,19 +16,22 @@ const [ Completes, setCompletes ] = useState([
 
 const onChangeTodoText = (event: React.ChangeEvent<HTMLInputElement>) => setTodoText(event.target.value);
 
+// タスクの追加
 const handleAddTodo = () => {
 if (TodoText === "") return;
 const newTodo = [...Incompletes, TodoText];
 setIncompletes(newTodo)
 setTodoText("")
-}
+};
 
+// タスクの削除
 const handleDelete = (index: number) => {
   const newTodo = [...Incompletes];
   newTodo.splice(index, 1);
   setIncompletes(newTodo);
-}
+};
 
+// タスクの完了
 const handleComplete = (index: number) => {
 const newIncompleteTodo = [...Incompletes];
 newIncompleteTodo.splice(index, 1);
@@ -36,7 +39,17 @@ newIncompleteTodo.splice(index, 1);
 const newCompleteTodo = [...Completes, Incompletes[index]];
 setIncompletes(newIncompleteTodo);
 setCompletes(newCompleteTodo);
-}
+};
+
+// タクスを戻す
+const handleRevert = (index: number) => {
+const newRevertComplete = [...Completes];
+newRevertComplete.splice(index, 1);
+
+const newRevertInComplete = [...Incompletes, Completes[index]];
+setCompletes(newRevertComplete);
+setIncompletes(newRevertInComplete);
+};
 
   return (
     <>
@@ -69,10 +82,11 @@ setCompletes(newCompleteTodo);
       <div className='mx-2 rounded-md border-2 bg-teal-400 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>完了のTODO</p>
         <ul className='flex-col items-center mx-2 gap-2'>
-          {Completes.map((todo) => (
+          {Completes.map((todo, index) => (
               <li key={todo} className='flex items-center gap-2'>
               <p className="flex-grow">{todo}</p>
-              <button className="bg-gray-200 my-2 hover:bg-gray-300">戻す</button>
+              <button onClick={() => handleRevert(index)} 
+              className="bg-gray-200 my-2 hover:bg-gray-300">戻す</button>
               </li>
           ))}
         </ul>
