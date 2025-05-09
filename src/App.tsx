6ebad19b7fa593f94cca9cf05c11a6e5253a06ed
torimugi <zoo.zoo.0903@outlook.jp>
2025-05-09 23:00:34 +0900

    add.tsxの名前をincompleteに変更する

--- src/App.tsx
+++ src/App.tsx
@@ -63,20 +63,7 @@ onChange={onChangeTodoText}
 onClick={handleAddTodo}
 />
 
-      <div className='mx-2 rounded-md border-2 border-teal-400 py-2'>
-        <p className='text-xl font-bold text-center'>未完了のTODO</p>
-        <ul className='flex-col items-center mx-2 gap-2'>
-          {Incompletes.map((todo , index) => (
-              <li key={todo} className='flex items-center gap-2'>
-              <p className="flex-grow">{todo}</p>
-              <button onClick={() => handleComplete(index)}
-              className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
-              <button onClick={() => handleDelete(index)} 
-              className="bg-gray-200 hover:bg-gray-300">削除</button>
-              </li>
-          ))}
-        </ul>
-      </div>
+
       
       <div className='mx-2 rounded-md border-2 bg-teal-400 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>完了のTODO</p>
