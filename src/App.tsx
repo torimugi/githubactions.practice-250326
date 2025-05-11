1700a5996c5a82414233e1e4415641e294f7c560
torimugi <zoo.zoo.0903@outlook.jp>
2025-05-12 06:43:02 +0900

    completeTodo.tsxをコンポーネント化（ファイル作成）

--- src/App.tsx
+++ src/App.tsx
@@ -72,18 +72,7 @@ handleDelete={handleDelete}
 
 
       
-      <div className='mx-2 rounded-md border-2 bg-teal-400 border-teal-400 py-2'>
-        <p className='text-xl font-bold text-center'>完了のTODO</p>
-        <ul className='flex-col items-center mx-2 gap-2'>
-          {Completes.map((todo, index) => (
-              <li key={todo} className='flex items-center gap-2'>
-              <p className="flex-grow">{todo}</p>
-              <button onClick={() => handleRevert(index)} 
-              className="bg-gray-200 my-2 hover:bg-gray-300">戻す</button>
-              </li>
-          ))}
-        </ul>
-      </div>
+
       </div>
     </div>
     </>
