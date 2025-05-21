68a89b167a3343d7300034feca2623226f687eaf
torimugi <zoo.zoo.0903@outlook.jp>
2025-05-22 06:46:28 +0900

    input.tsxファイルにpropsを受け取るようにする

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,7 +6,7 @@ type Props = {
   onClick: () => void;
 };
 
-export const InputTodo: React.FC<Props> = ({ TodoText, onChange, onClick }) => {
+export const InputTodo: React.FC<Props> = ({ TodoText, onChange, onClick, disabled }) => {
   return (
     <div className="flex rounded-md overflow-hidden bg-teal-400 p-2 w-full max-w-md">
       <input
@@ -15,10 +15,12 @@ export const InputTodo: React.FC<Props> = ({ TodoText, onChange, onClick }) => {
         className="flex-grow p-2 outline-none bg-white mr-2"
         value={TodoText}
         onChange={onChange}
+        disabled={disabled}
       />
       <button
         onClick={onClick}
         className="bg-gray-200 rounded-md px-4 py-2 hover:bg-gray-300"
+        disabled={disabled}
       >
         追加
       </button>
