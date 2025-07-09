5389a114cd38f6a66c38c286175a209f2aaeada9
torimugi <zoo.zoo.0903@outlook.jp>
2025-07-09 17:12:44 +0900

    実装を意識したモックの変更（修正）

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "tailwindcss";
 
 export const App = () => {
  const [ Incompletes, setIncompletes ] = useState(["TODOです1", "TODOです2"]);
-
+ const [ Completes, setCompletes ] = useState(["TODOでした1", "TODOでした2"]);
   return (
     <>
     <div className="flex flex-col gap-y-6">
@@ -26,7 +26,7 @@ export const App = () => {
       <div className='mx-2 rounded-md border-2 bg-teal-400 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>完了のTODO</p>
         <ul className='flex items-center mx-2 gap-2'>
-        {Incompletes.map((todo, index) => (
+        {Completes.map((todo, index) => (
         <li key={index} className='flex items-center gap-2'>
               <p className="flex-grow">{todo}</p>
               <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
