f00a5597d90b226bec483d5a94112d5a964535b8
torimugi <zoo.zoo.0903@outlook.jp>
2025-07-15 17:52:16 +0900

    「追加」ボタンで、TODOを追加する

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,18 @@ export const App = () => {
  const [ Completes, setCompletes ] = useState(["TODOでした1", "TODOでした2"]);
   
  const onChangeTodo = (e: React.ChangeEvent<HTMLInputElement>) => setTodoText(e.target.value);
+
+ const onClickAddTodo = () => {
+  setIncompletes([...Incompletes, todoText]);
+  setTodoText("");
+ };
  
  return (
     <>
     <div className="flex flex-col gap-y-6">
       <div className='mx-2 bg-teal-400 p-2 rounded-md gap-2 flex'>
         <input type="text" placeholder="TODOを入力" className="p-2 bg-white" value={todoText} onChange={onChangeTodo} />
-        <button className="bg-gray-200 rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300">追加</button>
+        <button className="bg-gray-200 rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300" onClick={onClickAddTodo}>追加</button>
       </div>
       <div className='mx-2 rounded-md border-2 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>未完了のTODO</p>
