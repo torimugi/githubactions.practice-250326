ba9d88ae2695966c2d432cb311eadfbbb13248ec
torimugi <zoo.zoo.0903@outlook.jp>
2025-07-15 17:28:18 +0900

    テキストボックスに入力した文字を反映させる

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import "tailwindcss";
-import { InputTodo } from "./components/input";
-import { IncompleteTodo } from "./components/incompleteTodo";
-import { CompleteTodo } from "./components/completeTodo";
+
 
 export const App = () => {
+ const [ todoText, setTodoText ] = useState("");
  const [ Incompletes, setIncompletes ] = useState(["TODOです1", "TODOです2"]);
- const [ Completes, setCompletes ] = useState(["todoでした1", "todoでした2"]);
-  return (
+ const [ Completes, setCompletes ] = useState(["TODOでした1", "TODOでした2"]);
+  
+ const onChangeTodo = (e: React.ChangeEvent<HTMLInputElement>) => setTodoText(e.target.value);
+ 
+ return (
     <>
     <div className="flex flex-col gap-y-6">
       <div className='mx-2 bg-teal-400 p-2 rounded-md gap-2 flex'>
-        <input type="text" placeholder="TODOを入力" className="p-2 bg-white"/>
-        <button className="bg-Qgray-200 rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300">追加</button>
+        <input type="text" placeholder="TODOを入力" className="p-2 bg-white" value={todoText} onChange={onChangeTodo} />
+        <button className="bg-gray-200 rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300">追加</button>
       </div>
       <div className='mx-2 rounded-md border-2 border-teal-400 py-2'>
         <p className='text-xl font-bold text-center'>未完了のTODO</p>
