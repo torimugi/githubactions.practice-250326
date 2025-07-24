34f3fc35c8e314ce4791bc79bf390ed118b265dc
torimugi <zoo.zoo.0903@outlook.jp>
2025-07-24 18:38:37 +0900

    モックに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "tailwindcss";
 
+
 export const App = () => {
+  const [ inputTodo, setInputTodo ] = useState(["TODO1", "TODO2"]);
+const [ completTodo, setCompletTodo ] = useState(["TODO1でした", "TODO2でした"]);
+
   return (
     <>
     <div className="flex flex-col gap-y-6">
@@ -12,32 +17,29 @@ export const App = () => {
       <div className='mx-2 rounded-md border-2 border-teal-400 py-2 min-h-100'>
         <p className='text-xl font-bold text-center my-2'>未完了のTODO</p>
         <ul className='flex-col items-center mx-2 gap-2'>
-              <li className='flex items-center gap-2'>
-              <p className="flex-grow">TODO1</p>
-              <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
-              <button className="bg-gray-200 hover:bg-gray-300">削除</button>
-              </li>
-              <li className='flex items-center gap-2'>
-              <p className="flex-grow">TODO2</p>
-              <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
-              <button className="bg-gray-200 hover:bg-gray-300">削除</button>
-              </li>
+        {inputTodo.map((todo) => {
+                return (
+                  <li key={todo} className='flex items-center gap-2'>
+                  <p className="flex-grow">{todo}</p>
+                  <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
+                  <button className="bg-gray-200 hover:bg-gray-300">削除</button>
+                  </li>
+                )
+                  })}
         </ul>
       </div>
       
       <div className='mx-2 rounded-md border-2 bg-teal-400 border-teal-400 py-2 min-h-100'>
         <p className='text-xl font-bold text-center my-2'>完了のTODO</p>
         <ul className='flex-col items-center mx-2 gap-2'>
-        <li className='flex items-center gap-2'>
-              <p className="flex-grow">TODO1でした</p>
-              <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
-              <button className="bg-gray-200 hover:bg-gray-300">削除</button>
-              </li>
-              <li className='flex items-center gap-2'>
-              <p className="flex-grow">TODO2でした</p>
-              <button className="bg-gray-200 my-2 hover:bg-gray-300">完了</button>
-              <button className="bg-gray-200 hover:bg-gray-300">削除</button>
-              </li>
+        {completTodo.map((todo) => {
+                return (
+                  <li key={todo} className='flex items-center gap-2'>
+                  <p className="flex-grow">{todo}</p>
+                  <button className="bg-gray-200 my-2 hover:bg-gray-300">戻す</button>
+                  </li>
+                )
+                  })}
         </ul>
       </div>
     </div>
